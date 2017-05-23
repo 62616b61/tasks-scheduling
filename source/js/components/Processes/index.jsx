ecf4819d813d4b736b6f8630f9bccb919f8bf139
Alexander Katsero <alexanderkatsero@gmail.com>
2017-05-23 23:06:01 +0300

    removed redundant component

--- source/js/components/Processes/index.jsx
+++ source/js/components/Processes/index.jsx
@@ -1,16 +1,34 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Flex, Box } from 'reflexbox'
-import ProcessesTable from './ProcessesTable'
+import { Table, Progress } from 'rebass'
 
 const Processes = (props) => {
+  const tableData = props.processes.map((proc) => {
+    const cputime = proc.cputime
+    const done = cputime.done / cputime.required
+
+    return ([
+      proc.id,
+      <Progress color='primary' value={done} />,
+      cputime.done + '/' + cputime.required
+    ])
+  })
+
   return (
     <Flex>
       <Box
         col={12}
       >
         <h5>Processes</h5>
-        <ProcessesTable processes={props.current} />
+        <Table
+          data={tableData}
+          headings={[
+            '#',
+            'Progress',
+            ''
+          ]}
+        />
       </Box>
     </Flex>
   )
@@ -19,7 +37,7 @@ const Processes = (props) => {
 export default connect(
   (state) => {
     return {
-      current: state.processes.list.current
+      processes: state.processes.list.current
     }
   }
 )(Processes)
