ea503641a9bec6cb63c1fe0cf92bb225ada9dbc5
Alexander Katsero <alexanderkatsero@gmail.com>
2017-05-23 19:36:49 +0300

    display seconds passed

--- source/js/ducks/timer.js
+++ source/js/ducks/timer.js
@@ -1,19 +1,24 @@
+const TIMER_START = 'timer/START'
 const TIMER_TICK = 'timer/TICK'
 
-export function Tick () {
-  return (dispatch) => {
-    dispatch({type: TIMER_TICK})
-  }
-}
+export const Tick = () => ({type: TIMER_TICK})
+export const Start = () => ({type: TIMER_START})
 
 const INITIAL_STATE = {
+  start: null,
   ticks: 0
 }
 
 export default function timerReducer (state = INITIAL_STATE, action) {
   switch (action.type) {
+    case TIMER_START:
+      return {
+        ...state,
+        start: new Date()
+      }
     case TIMER_TICK:
       return {
+        ...state,
         ticks: state.ticks + 1
       }
     default:
