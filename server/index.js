bd5f7ccb9f642b5830b917e8e2e3a0af28d47837
whynotcrybot <alexanderkatsero@gmail.com>
2017-08-14 22:51:29 +0300

    minor changes

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 
 const app = express()
 
@@ -8,9 +8,9 @@ const port = process.env.PORT || 3000
 const outputPath = path.resolve(process.cwd(), 'dist')
 const publicPath = '/'
 
-app.use(publicPath, express.static(outputPath));
+app.use(publicPath, express.static(outputPath))
 
-app.get('*', (req, res) => res.sendFile(path.resolve(publicPath, 'index.html')));
+app.get('*', (req, res) => res.sendFile(path.resolve(publicPath, 'index.html')))
 
 app.listen(port, (err) => {
   console.log('boop')
