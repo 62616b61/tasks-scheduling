b52c2b4788fd26b5c853a08c50f205e3ea49fa44
whynotcrybot <alexanderkatsero@gmail.com>
2017-08-14 22:32:28 +0300

    removed host setting

--- server/index.js
+++ server/index.js
@@ -4,16 +4,14 @@ const path = require('path');
 const app = express()
 
 const port = process.env.PORT || 3000
-const host = 'localhost'
 
 const outputPath = path.resolve(process.cwd(), 'dist')
 const publicPath = '/'
-
 
 app.use(publicPath, express.static(outputPath));
 
 app.get('*', (req, res) => res.sendFile(path.resolve(publicPath, 'index.html')));
 
-app.listen(port, host, (err) => {
+app.listen(port, (err) => {
   console.log('boop')
 });
