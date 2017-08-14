17e56f16205328463097585d7a66bfe3a086d2ee
whynotcrybot <alexanderkatsero@gmail.com>
2017-08-14 21:59:26 +0300

    build when started

--- package-scripts.js
+++ package-scripts.js
@@ -15,7 +15,8 @@ module.exports = {
     },
     default: {
       description: 'Start project with pm2 on production.',
-      script: `${crossEnv('NODE_ENV=production')} node server/`
+      default: series.nps('build', 'default.start'),
+      start: `${crossEnv('NODE_ENV=production')} node server/`
     },
     dev: {
       default: 'webpack-dev-server --config ./webpack/webpack.config.babel.js'
